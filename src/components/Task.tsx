60e44af2e9d9c05573829caeaef13e0972a2417b
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-02-18 16:02:31 -0300

    tasks completed, check tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,35 @@
 import styles from './Task.module.css'
 import { Trash } from 'phosphor-react';
+import { useState } from 'react';
+import {BsCheckCircleFill, BsCircle} from "react-icons/bs";
+
+interface TaskProps {
+    id: string,
+    title: string,
+    isCompleted: boolean
+    checked: boolean
+    handleToggle: (id:string) => void
+}
 
 
-export function Task(props: {title:string}){
+export function Task({id, title, isCompleted, checked, handleToggle}: TaskProps){
+    const [isCheck, setIsCheck] = useState(false);
+
+    function handleClick()  {
+    setIsCheck(!isCheck)
+    handleToggle(id)
+    }
+    
     return (
-        <div className={styles.task}>
-            
-            <div className={styles.checkboxWrapper}>
-                <input className={styles.checkMark} type="checkbox" name="mailing" id="task"/> 
-                <label htmlFor="task"> </label>               
-            </div>
-               
-            <p>{props.title}</p>
+        <div className={styles.task}   >
+
+            <div onClick={() => handleClick()}> 
+                {isCheck ? <BsCheckCircleFill className={styles.taskChecked}/> : <BsCircle className={styles.checkUnchecked}/>}
+             </div>
+
+            <span className={isCheck ? styles.textCheck : undefined} onClick={() => handleClick()}>{title}</span>
             <div>
-            <Trash size={20}/>
+            <Trash className={styles.trash} size={20}/>
             </div>
             
         </div>
